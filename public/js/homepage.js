69fe9b829d127e9720b1d55f9e0d7d5672cafbb4
Brian-Lascuna <blascuna@gmail.com>
2023-05-15 19:59:58 -0700

    added a page to post to the blog, functionality WIP

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,5 +1,6 @@
 const loginBtn = document.querySelector('#login-btn');
 const signUpBtn = document.querySelector('#signup-btn');
+const postBtn = document.querySelector('#post-btn');
 
 function redirectToLogin() {
     document.location.replace('/login');
@@ -9,5 +10,10 @@ function redirectToSignUp() {
     document.location.replace('/signup');
 }
 
+function redirectToPostPage() {
+    document.location.replace('/blog-post')
+}
+
 if (loginBtn) {loginBtn.addEventListener('click', redirectToLogin);}
 if (signUpBtn) {signUpBtn.addEventListener('click', redirectToSignUp);}
+if (postBtn) {postBtn.addEventListener('click', redirectToPostPage)};
